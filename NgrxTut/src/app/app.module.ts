bcec1f629daede20fc357e34ab562edbc8477fa6
Ti43652 <47326833+Ti43652@users.noreply.github.com>
2021-03-24 02:00:14 +0000

    Week Finished

---
+++ NgrxTut/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
+
+import { StoreModule } from '@ngrx/store';
+import {reducer} from './reducers/tutorial.reducers';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReadComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({
+      tutorial: reducer
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
